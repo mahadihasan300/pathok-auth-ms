95949de78e2133abb7a0b7276e151dd49c959675
Mahadi Hasan <md.mahadihasan300@gmail.com>
2024-05-31 21:00:55 +0600

    banner added

--- src/main/java/com/example/springJwt/controller/DemoController.java
+++ src/main/java/com/example/springJwt/controller/DemoController.java
@@ -9,7 +9,12 @@ public class DemoController {
 
     @GetMapping("/demo")
     public ResponseEntity<String> demo() {
-        return ResponseEntity.ok("Hello from secured url");
+        return ResponseEntity.ok("Hello from non secured url");
+    }
+
+    @GetMapping("/securedDemo")
+    public ResponseEntity<String> securedDemo() {
+        return ResponseEntity.ok("Hello from non secured url");
     }
 
     @GetMapping("/admin_only")
